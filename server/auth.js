5217d24da1d0c1dc296410a639f4c5eee6d3cf2f
Raunak-Shalya <raunakshalya20@ece.iiitp.ac.in>
2023-06-20 00:08:10 +0530

    Completed Google Auth. Integration

--- server/auth.js
+++ server/auth.js
@@ -8,13 +8,11 @@ var GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
     clientID:     process.env.CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://yourdomain:3000/auth/google/callback",
+    callbackURL: "http://localhost:3000/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
+    done(null,profile);
   }
 ));
 
--- server/server.js
+++ server/server.js
@@ -1,28 +1,35 @@
-
 const express=require('express')
 const passport=require('passport')
 const path=require('path')
 const app=express();
-
 require('./auth');
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'client')))
 
+var session = require('express-session')
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email' , 'name'] }));
+
+
+
+app.get('/login', (req,res) =>{
+    res.redirect('/');
+})
+
+
+  app.use(session({
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
 
 app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/homepage');
-  });
+  passport.authenticate('google', { failureRedirect: '/login' , successRedirect: '/homepage'}));
 
 
 app.listen(3000, ()=>{
