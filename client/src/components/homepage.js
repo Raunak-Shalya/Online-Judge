9480ad24be89e3a189e4270905b8029613f8504a
Raunak-Shalya <raunakshalya20@ece.iiitp.ac.in>
2023-06-26 22:48:43 +0530

    Passed problems to frontend using Axios

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './NavBar'
 import '../styles/homepage.css'
+import axios from 'axios';
 const Homepage = () => {
+
+ const [problems,setproblems]=useState([]);
+  
+  useEffect(()=>{
+    try {
+      axios.get("http://localhost:5000/getproblems")
+       .then((res) => setproblems(res.data));
+    } catch (error) {
+      console.log("Error in displaying problems at frontend")
+    }
+  },[])
   return (
     <>
     <Navbar/>
-    <div>homepage</div>
+    <div>
+    {problems.map((problem)=>{
+      const { Pid, PS, D } = problem;
+      return(
+        <>
+          <h>{Pid}</h>
+          <h1>{PS}</h1>
+          <a>{D}</a>
+        </>
+      )
+      })}
+    </div>
     </>
   )
 }
